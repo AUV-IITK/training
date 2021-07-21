276ad0418a83c22d145afa293978d45a7f181a9b
Kshubham20 <kshubham20@iitk.ac.in>
2021-07-21 22:42:25 +0530

    assignment2 submission

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/shubham/catkin_ws/src
+# Build directory: /home/shubham/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("new")
+subdirs("demo_bot_gazebo")
+subdirs("diff_drive_description")
---
+++ catkin_ws/build/diff_drive_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/shubham/catkin_ws/src/diff_drive_description
+# Build directory: /home/shubham/catkin_ws/build/diff_drive_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
